7886b84a40fbb85bf74f78671facbcf1585671f4
tainiikrab <60309219+tainiikrab@users.noreply.github.com>
2025-11-24 02:48:12 +0300

    First chem stuff

---
+++ Assets/GameAssets/Scripts/IDraggable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public interface IDraggable
+{
+    void OnGrab(Vector2 targetPos);
+    void OnDrag(Vector2 targetPos);
+    void OnRelease();
+
+    void OnRotateHold();
+    void OnRotateRelease();
+}
