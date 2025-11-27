fa101296b4bba53c09ca6d76b771ef5f12dfa83f
tainiikrab <60309219+tainiikrab@users.noreply.github.com>
2025-11-27 14:57:23 +0300

    Particle coloring

---
+++ Assets/GameAssets/Scripts/ParticleProperties.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ParticleProperties : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    public Color Color
+    {
+        get => spriteRenderer.color;
+        set => spriteRenderer.color = value;
+    }
+
+
+#if UNITY_EDITOR
+    [SerializeField] private Color initialColor;
+    private void OnValidate()
+    {
+        if (spriteRenderer != null) spriteRenderer.color = initialColor;
+    }
+#endif
+}
--- Assets/GameAssets/Scripts/ParticleReleaser.cs
+++ Assets/GameAssets/Scripts/ParticleReleaser.cs
@@ -3,25 +3,26 @@ using UnityEngine;
 
 public class ParticleReleaser : MonoBehaviour
 {
-    [SerializeField] private String layer;
+    [SerializeField] private string particleTag;
     [SerializeField] private Transform particleHolder;
-    void Start()
+
+    private void Awake()
     {
-        
+        particleTag = GlobalValues.particleTag;
+    }
+
+    private void Start()
+    {
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
     }
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Debug.Log(other.gameObject.layer.ToString());
-        if (other.gameObject.layer == LayerMask.NameToLayer(layer));
-        {
-            other.transform.parent = particleHolder;
-        }
+        if (other.CompareTag(particleTag)) other.transform.parent = particleHolder;
     }
 }
