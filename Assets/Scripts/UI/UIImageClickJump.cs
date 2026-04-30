3822bb24bdbfbb4daf47ca95f6c9711fe323e132
tainiikrab <60309219+tainiikrab@users.noreply.github.com>
2026-05-01 00:28:54 +0300

    Update main menu volume and flavour

---
+++ Assets/Scripts/UI/UIImageClickJump.cs
@@ -0,0 +1,52 @@
+using PrimeTween;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// Подпрыгивание UI по клику: быстрый взлёт и мягкое приземление с отскоком через PrimeTween
+/// </summary>
+[RequireComponent(typeof(Image))]
+public class UIImageClickJump : MonoBehaviour, IPointerClickHandler
+{
+    [Header("Jump")] 
+    [SerializeField] private float jumpHeight = 28f;
+    [SerializeField] private float riseDuration = 0.12f;
+    [SerializeField] private float fallDuration = 0.48f;
+    [SerializeField] private Ease riseEase = Ease.OutQuad;
+    [SerializeField] private Ease fallEase = Ease.OutBounce;
+
+    private RectTransform _rect;
+    private Vector2 _baseAnchoredPosition;
+    private Sequence _jump;
+
+    private void Awake()
+    {
+        _rect = (RectTransform)transform;
+        _baseAnchoredPosition = _rect.anchoredPosition;
+    }
+
+    private void OnDisable()
+    {
+        if (_jump.isAlive)
+            _jump.Stop();
+
+        if (_rect != null)
+            _rect.anchoredPosition = _baseAnchoredPosition;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_jump.isAlive)
+            return;
+
+        Tween.StopAll(_rect);
+        _rect.anchoredPosition = _baseAnchoredPosition;
+
+        Vector2 apex = _baseAnchoredPosition + new Vector2(0f, jumpHeight);
+
+        _jump = Sequence.Create()
+            .Chain(Tween.UIAnchoredPosition(_rect, apex, riseDuration, riseEase))
+            .Chain(Tween.UIAnchoredPosition(_rect, _baseAnchoredPosition, fallDuration, fallEase));
+    }
+}
